b273abac255f5e610f0b4ed00ae05030dbb12c93
thisisloginid <thisisloginid@gmail.com>
2017-06-10 20:22:08 +0900

    third commit

--- app/src/main/java/com/example/neo/andstudy/MainActivity.java
+++ app/src/main/java/com/example/neo/andstudy/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.neo.andstudy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.idText:
+                Toast.makeText(getApplicationContext(), "message", Toast.LENGTH_SHORT).show();
+        }
     }
 }
